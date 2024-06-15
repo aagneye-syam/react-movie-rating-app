c38d4285c2843180e172db79e416476d79882b22
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 00:03:07 +0530

    navbar code changed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './Navbar.css'
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
-function Navbar() {
+export const Navbar = () => {
   return (
-    <div className='navbar'>
-       <img className='logo' src="" alt="SBPN" />
-       <img className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
-    </div>
-  )
-}
-
-export default Navbar
+    <Menu fixed="top" size="huge">
+      <Menu.Menu position="right">
+        <Menu.Item as={Link} to="/auth">
+          {" "}
+          Auth{" "}
+        </Menu.Item>
+      </Menu.Menu>
+    </Menu>
+  );
+};
