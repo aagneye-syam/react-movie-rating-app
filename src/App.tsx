2e8c0b2e604c0f16fb5624ff67c41d6f045c4313
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 00:06:48 +0530

    semantic css err resolved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <div>
-      <Navbar/>
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<h1>HOME</h1>}/>
-          <Route path="/auth" element={<h1>AUTH</h1>}/>
-          <Route path="/rated" element={<h1>RATED</h1>}/>
+          <Route path="/" element={<h1>HOME</h1>} />
+          <Route path="/auth" element={<h1>AUTH</h1>} />
+          <Route path="/rated" element={<h1>RATED</h1>} />
         </Routes>
       </Router>
     </div>
