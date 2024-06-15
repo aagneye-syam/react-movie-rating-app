c3725bc47a3af320608db92c04de7ba0a5265754
aagneye-syam <saagneye2003@gmail.com>
2024-06-15 23:58:35 +0530

    imported style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
-  return (<></>)
+  return (
+    <div>
+      <Navbar/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<h1>HOME</h1>}/>
+          <Route path="/auth" element={<h1>AUTH</h1>}/>
+          <Route path="/rated" element={<h1>RATED</h1>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;
