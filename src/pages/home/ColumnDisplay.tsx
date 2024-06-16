ae1a1eeb3f4252ab30c799163e361436e77e25ac
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 11:40:01 +0530

    Props def in Column display

--- src/pages/home/ColumnDisplay.tsx
+++ src/pages/home/ColumnDisplay.tsx
@@ -1,3 +1,17 @@
-export const ColumnDisplay = () => {
-        
+import {DisplayType} from ".";
+
+interface DisplayData {
+  id: number;
+  overview: string;
+  poster_path: string;
+  title: string;
+  vote_average: number;
+  release_date: string;
 }
+
+interface Props {
+  data: DisplayData[];
+  displayType: DisplayType;
+}
+
+export const ColumnDisplay = (props:Props) => {};
