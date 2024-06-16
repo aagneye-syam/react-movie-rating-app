0ce80e9aa8f8ee3bef2e6b86095406394b78d8eb
aagneye-syam <saagneye2003@gmail.com>
2024-06-16 08:14:13 +0530

    movie and tvShow toggle btn in Home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/Navbar";
 import { Auth } from "./pages/auth";
+import { Home } from "./pages/home";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>HOME</h1>} />
+          <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/rated" element={<h1>RATED</h1>} />
         </Routes>
